f89ae5b289b1f0aab60c92f3c7c3075a3f56cca1
Auntapza <chatdanai.k7563@gmail.com>
2023-04-09 22:50:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 const api_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cdogecoin%2Cethereum&vs_currencies=thb&include_24hr_change=true"
 
-fetch(api_url)
-    .then(res => res.json())
-    .then(json => {
-        // console.log(json)
-        const container = document.querySelector(".container");
-        const coins = Object.getOwnPropertyNames(json)
-        
-        for (let coin of coins) {
-            const coinInfo = json[`${coin}`]
-            const price = coinInfo.thb
-            let change = coinInfo.thb_24h_change
+async function getdata() {
+    let res = await fetch(api_url)
+    let json = await res.json()
+    // console.log(json)
+    const container = document.querySelector(".container");
+    const coins = Object.getOwnPropertyNames(json)
+    container.innerHTML = ''
+    
+    for (let coin of coins) {
+        const coinInfo = json[`${coin}`]
+        const price = coinInfo.thb
+        let change = coinInfo.thb_24h_change
 
-            container.innerHTML += `
-                <div class="coin ${change < 0 ? 'falling' : 'rising'}">
-                    <div class="coin-logo">
-                        <img src="image/${coin}.png" alt="${coin}">
-                    </div>
-                    <div class="coin-name">
-                        <h3>${coin}</h3>
-                        <span>/THB</span>
-                    </div>
-                    <div class="coin-price">
-                        <span class="price">${price} THB</span>
-                        <span class="change">${change < 0 ? '' : '+'}${change}</span>
-                    </div>
+        container.innerHTML += `
+            <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+                <div class="coin-logo">
+                    <img src="image/${coin}.png" alt="${coin}">
                 </div>
-            `
+                <div class="coin-name">
+                    <h3>${coin}</h3>
+                    <span>/THB</span>
+                </div>
+                <div class="coin-price">
+                    <span class="price">${price} THB</span>
+                    <span class="change">${change < 0 ? '' : '+'}${change}</span>
+                </div>
+            </div>
+        `
 
-        }
-    })
+    }
+    // console.log('new')
+}
+
+window.onload = () => {
+    getdata()
+}
+
+setInterval(() => {
+    location.reload()
+}, 5000)
